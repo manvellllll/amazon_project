a17dcac492bace0f617094433f56e82a6d7334e3
Manvel <mbagratyan87@gmail.com>
2025-06-12 15:34:52 +0400

    adding the quantity selector function

--- js/amazon.js
+++ js/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -67,16 +67,25 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
                 matchingItem = item;
             }
         });
+        
+        const quantitySelector = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
         if (matchingItem) {
-            matchingItem.quantity ++;
+            matchingItem.quantity += quantitySelector;
         } else {
             cart.push({
             productId: productId,
-            quantity: 1
+            quantity: quantitySelector
         });
         }
 
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     
         console.log(cart);
     });
