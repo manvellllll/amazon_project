e146183b38923340ea6abe786067d294a9d365be
Manvel <mbagratyan87@gmail.com>
2025-07-07 14:49:58 +0400

    finishing the checkout page

--- js/checkout/orderSummary.js
+++ js/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import { calculateCartQuantity, cart, removeFromCart, updateDeliveryOption, upda
 import { getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
@@ -17,9 +17,7 @@ export function renderOrderSummary() {
 
         let deliveryOption = getDeliveryOption(deliveryOptionId);
 
-        const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-        const dateString = deliveryDate.format('dddd, MMMM D');
+        const dateString = calculateDeliveryDate(deliveryOption);
 
         cartSummaryHTML += `
             <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -70,9 +68,7 @@ export function renderOrderSummary() {
         let html = '';
 
         deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-            const dateString = deliveryDate.format('dddd, MMMM D');
+            const dateString = calculateDeliveryDate(deliveryOption)
 
             const priceString = deliveryOption.priceCents
             === 0
@@ -144,8 +140,7 @@ export function renderOrderSummary() {
             const productId = link.dataset.productId;
             removeFromCart(productId);
         
-            const container = document.querySelector(`.js-cart-item-container-${productId}`);
-            container.remove();
+            renderOrderSummary();
             cartHeaderQuantity();
             renderPaymentSummary();
         });
--- js/checkout/paymentSummary.js
+++ js/checkout/paymentSummary.js
@@ -16,6 +16,11 @@ export function renderPaymentSummary() {
     const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
     const taxCents = totalBeforeTaxCents * 0.1;
     const totalCents = totalBeforeTaxCents + taxCents;
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+
 
     const paymentSummaryHTML = `
         <div class="payment-summary-title">
@@ -23,32 +28,42 @@ export function renderPaymentSummary() {
         </div>
 
         <div class="payment-summary-row">
-        <div>Items (3):</div>
-        <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
+            <div>Items (${cartQuantity}):</div>
+            <div class="payment-summary-money">
+                $${formatCurrency(productPriceCents)}
+            </div>
         </div>
 
         <div class="payment-summary-row">
-        <div>Shipping &amp; handling:</div>
-        <div class="payment-summary-money">$${formatCurrency(shippingPriceCents)}</div>
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">
+                $${formatCurrency(shippingPriceCents)}
+            </div>
         </div>
 
         <div class="payment-summary-row subtotal-row">
-        <div>Total before tax:</div>
-        <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">
+                $${formatCurrency(totalBeforeTaxCents)}
+            </div>
         </div>
 
         <div class="payment-summary-row">
-        <div>Estimated tax (10%):</div>
-        <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">
+                $${formatCurrency(taxCents)}
+            </div>
         </div>
 
         <div class="payment-summary-row total-row">
-        <div>Order total:</div>
-        <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
+            <div>Order total:</div>
+            <div class="payment-summary-money">
+                $${formatCurrency(totalCents)}
+            </div>
         </div>
 
         <button class="place-order-button button-primary">
-        Place your order
+            Place your order
         </button>
     `;
 
