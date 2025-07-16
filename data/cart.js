bd02d0720e0529892c3f7f008b68659508e298cb
Manvel <mbagratyan87@gmail.com>
2025-07-16 22:43:57 +0400

    adding jasmine tests for addToCart function

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,49 @@
-export let cart = JSON.parse(localStorage.getItem('cart'))
+export let cart;
 
-if (!cart) {
+loadFromStorage();
 
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
     cart = [{
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: '1'
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
     }, {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId: '2'
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
     }];
+  }
 }
-
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export function addToCart (productId) {
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
+  }
+
+  saveToStorage();
+}
+
+/*export function addToCart (productId) {  //This addToCart function has more func but unable to test
     let matchingItem;
     cart.forEach((cartItem) => {
         if (productId === cartItem.productId) {
@@ -38,7 +64,7 @@ export function addToCart (productId) {
         });
     }
     saveToStorage();
-}
+}*/
 export function removeFromCart(productId) {
     const newCart = [];
 
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../js/utils/money.js";
+import { formatCurrency } from "../../js/utils/money.js";
 
 describe('test suite: formatCurrency', () => {
     it('converts cents into dollars', () => {
